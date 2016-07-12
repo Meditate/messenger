2bd94dcca42902849a4a809c1eebd6949aff0252
Meditate <aroundofyou@gmail.com>
2016-07-12 22:40:27 +0200

    Add attachment to mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,8 @@ class UserMailer < ApplicationMailer
 
   def user_email(email,email_params)
     @email=email
+    @file=email_params[:file]
+    attachments[@file.original_filename]=File.read("#{@file.path}")
     @content=email_params[:content]
     mail(to: @email, subject: 'Admin mail')
   end
