63558a04d7e9b589b61902b16e59545f4c103f62
Meditate <aroundofyou@gmail.com>
2016-07-14 16:48:58 +0200

    Add more styling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,9 @@ class UserMailer < ApplicationMailer
   def user_email(email,email_params)
     @email=email
     @file=email_params[:file]
-    attachments[@file.original_filename]=File.read("#{@file.path}")
+    if @file
+      attachments[@file.original_filename]=File.read("#{@file.path}")
+    end
     @content=email_params[:content]
     mail(to: @email, subject: 'Admin mail')
   end
